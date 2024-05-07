dcc1c022ee71d9486df162828c5b973ce3be8264
irwingato <atoseiji@gmail.com>
2024-05-07 09:41:24 -0300

    Terceiro commit com melhoramentos que usa a biblioteca crypto para gerar números aletórios mais uniformemente distribuídos em um intervalo de valores, e aproveitei api especializadas como crypto.randomBytes para gerar chaves aleatórias de forma mais eficiente e uniforme

--- btc.js
+++ btc.js
@@ -2,7 +2,7 @@ const { Worker, isMainThread } = require('worker_threads');
 const CoinKey = require('coinkey');
 const os = require('os');
 
-const wallets = ['13zb1hQbWVsc2S7ZTZnP2G4undNNpdh5so'];
+const wallets = ['1AWCLZAjKbV1P7AHvaPNCKiB7ZWVDMxFiz'];
 const numCPUs = os.cpus().length;
 const numThreads = Math.min(4, numCPUs); // Use no máximo 4 threads, ou o número de CPUs disponíveis, o que for menor
 
@@ -22,8 +22,16 @@ if (isMainThread) {
         new Worker(__filename);
     }
 } else {
+    // Função para gerar chaves aleatórias de forma mais uniforme
+    function generateRandomPrivateKeyUniform() {
+        // Gera um número aleatório entre 0 e 2^256 - 1
+        const randomBytes = require('crypto').randomBytes(32);
+        const privateKey = BigInt(`0x${randomBytes.toString('hex')}`);
+        return privateKey.toString(16).padStart(64, '0');
+    }
+
     while (true) {
-        const privateKey = generateRandomPrivateKey();
+        const privateKey = generateRandomPrivateKeyUniform(); // Usar a nova função de geração de chaves
         const publicKey = generatePublic(privateKey);
         if (wallets.includes(publicKey)) {
             console.log(`Chave privada encontrada: ${privateKey}`);
