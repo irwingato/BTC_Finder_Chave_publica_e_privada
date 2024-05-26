3f5ece0d8a91032a4abe76f16d731ac67301f87e
irwingato <atoseiji@gmail.com>
2024-05-26 15:13:54 -0300

    Adicionando o código para descriptografar a chave privada ou seja chave wif e melhorando o código para usar worker e threads

--- btc.js
+++ btc.js
@@ -1,18 +1,15 @@
-const { Worker, isMainThread, workerData } = require('worker_threads');
+const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
 const fs = require('fs');
 const CoinKey = require('coinkey');
 const os = require('os');
-const cluster = require('cluster');
+const crypto = require('crypto');
 
 const wallets = ['1AWCLZAjKbV1P7AHvaPNCKiB7ZWVDMxFiz'];
 const numCPUs = os.cpus().length;
 const numThreads = Math.min(4, numCPUs); // Use no máximo 4 threads, ou o número de CPUs disponíveis, o que for menor
 
-function generateRandomPrivateKey() {
-    const randomBytes = require('crypto').randomBytes(32);
-    const privateKey = BigInt(`0x${randomBytes.toString('hex')}`);
-    return privateKey.toString(16).padStart(64, '0');
-}
+const min = BigInt('0x2a90675934c000000');
+const max = BigInt('0x3ffffffffffffffff');
 
 function generatePublic(privateKey) {
     const key = new CoinKey(Buffer.from(privateKey, 'hex'));
@@ -34,43 +31,80 @@ function loadLastAttempt() {
     }
 }
 
-if (isMainThread) {
-    if (cluster.isMaster) {
-        // Cria workers com o mesmo script
-        for (let i = 0; i < numThreads; i++) {
-            cluster.fork();
-        }
-        // Se um worker morrer, cria um novo
-        cluster.on('exit', (worker, code, signal) => {
-            console.log(`Worker ${worker.process.pid} morreu`);
-            cluster.fork();
-        });
-    } else {
-        let lastAttempt = loadLastAttempt();
-        if (!lastAttempt) {
-            console.log('Não foi possível carregar a última tentativa. Iniciando uma nova busca...');
-        } else {
-            console.log('Retomando a busca a partir da última tentativa:', lastAttempt);
-        }
+function createWorker(start, end, lastAttempt) {
+    return new Worker(__filename, {
+        workerData: { start, end, wallets, lastAttempt }
+    });
+}
 
-        while (true) {
-            const privateKey = generateRandomPrivateKey();
-            if (privateKey === lastAttempt) {
-                console.log('Retomando a busca a partir do ponto salvo.');
-                lastAttempt = null;
-            }
-            saveAttempt(privateKey);
-            const publicKey = generatePublic(privateKey);
-            if (wallets.includes(publicKey)) {
-                console.log(`Chave privada encontrada: ${privateKey}`);
-                console.log(`Chave pública correspondente: ${publicKey}`);
-                process.exit();
-            }
-            if (!lastAttempt) {
-                saveAttempt(privateKey);
-            }
-        }
+if (isMainThread) {
+    console.log(`Master ${process.pid} is running`);
+
+    let lastAttempt = loadLastAttempt();
+    if (!lastAttempt) {
+        console.log('Não foi possível carregar a última tentativa. Iniciando uma nova busca...');
+    } else {
+        console.log('Retomando a busca a partir da última tentativa:', lastAttempt);
     }
+
+    const range = (max - min) / BigInt(numThreads);
+    const startWorkers = () => {
+        for (let i = 0; i < numThreads; i++) {
+            const start = min + BigInt(i) * range;
+            const end = (i === numThreads - 1) ? max : start + range - BigInt(1);
+            const worker = createWorker(start, end, lastAttempt);
+
+            worker.on('message', (message) => {
+                if (message.found) {
+                    console.log(`Chave privada encontrada: ${message.privateKey}`);
+                    console.log(`Chave pública correspondente: ${message.publicKey}`);
+                    process.exit();
+                }
+            });
+
+            worker.on('exit', (code) => {
+                if (code !== 0) {
+                    console.error(`Worker stopped with exit code ${code}`);
+                    // Restart the worker
+                    const newWorker = createWorker(start, end, lastAttempt);
+                    newWorker.on('message', (message) => {
+                        if (message.found) {
+                            console.log(`Chave privada encontrada: ${message.privateKey}`);
+                            console.log(`Chave pública correspondente: ${message.publicKey}`);
+                            process.exit();
+                        }
+                    });
+                }
+            });
+        }
+    };
+
+    startWorkers();
 } else {
-    console.log(`Worker ${process.pid} iniciado`);
+    const { start, end, wallets, lastAttempt } = workerData;
+
+    console.log(`Worker ${process.pid} started`);
+
+    (async () => {
+        const increment = BigInt(1);
+        const walletSet = new Set(wallets);
+
+        let key = lastAttempt ? BigInt('0x' + lastAttempt) + increment : start;
+        while (true) {
+            while (key <= end) {
+                const privateKey = key.toString(16).padStart(64, '0');
+                saveAttempt(privateKey);
+
+                const publicKey = generatePublic(privateKey);
+                if (walletSet.has(publicKey)) {
+                    parentPort.postMessage({ found: true, privateKey, publicKey });
+                    return;
+                }
+
+                key += increment;
+            }
+
+            key = start; // Reset key to start if the end is reached
+        }
+    })();
 }
